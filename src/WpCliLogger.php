182ad291986e458477ecb78a69fe43636e29f993
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2021-04-27 18:16:57 +0200

    New WCliLogger, that uses WP_CLI logging functions

---
+++ src/WpCliLogger.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace WonderWp\Component\Logging;
+
+class WpCliLogger extends AbstractLogger implements LoggerInterface
+{
+    public function error($message, array $context = [])
+    {
+        \WP_CLI::error($this->withTime($message));
+    }
+
+    public function warning($message, array $context = [])
+    {
+        \WP_CLI::warning($this->withTime($message));
+    }
+
+    public function success($message, array $context = [])
+    {
+        \WP_CLI::success($this->withTime($message));
+    }
+
+    public function debug($message, array $context = [])
+    {
+        \WP_CLI::debug($this->withTime($message));
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        \WP_CLI::log($this->withTime($message));
+    }
+
+}
